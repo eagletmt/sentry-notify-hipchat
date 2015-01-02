80bc0c7fe56b08ee143d124b322a4a29153494d0
Kohei Suzuki <eagletmt@gmail.com>
2015-01-02 11:57:15 +0900

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sentry-notify-hipchat',
-    version='0.0.1',
+    version='0.1.0',
     description='A Sentry notification plugin for HipChat',
     author='Kohei Suzuki',
     author_email='eagletmt@gmail.com',
