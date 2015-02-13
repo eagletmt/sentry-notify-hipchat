1ba29db98ec1e6debe8ad4dc0e57c72ad12437cb
Kohei Suzuki <eagletmt@gmail.com>
2015-02-13 17:52:20 +0900

    Revert "Start migration to v2 plugin"
    This reverts commit c78292b39719834aabf84ab3abebac0d6952b532.

--- sentry_notify_hipchat/plugin.py
+++ sentry_notify_hipchat/plugin.py
@@ -1,6 +1,6 @@
 from django import forms
 from hipchat import HipChat
-from sentry.plugins import Plugin2, Notifier
+from sentry.plugins import Plugin
 from sentry.utils.strings import strip
 import xml.sax.saxutils as saxutils
 
@@ -13,11 +13,13 @@ DEFAULT_HIPCHAT_ENDPOINT = 'https://api.hipchat.com/v1/'
 class NotifyHipchatForm(forms.Form):
     token = forms.CharField(help_text='HipChat API v1 token')
     room = forms.CharField(help_text='Room name or ID')
+    new_only = forms.BooleanField(help_text='Notify new messages only', required=False)
+    ignore_muted = forms.BooleanField(help_text="Don't notify muted events", required=False, initial=True)
     notify = forms.BooleanField(help_text='Enable "notify" flag', required=False, initial=True)
     endpoint = forms.CharField(help_text='HipChat API endpoint', required=False)
 
 
-class NotifyHipchatPlugin(Plugin2):
+class NotifyHipchatPlugin(Plugin):
     title = 'HipChat Notification'
     slug = 'notify-hipchat'
     description = 'A notification plugin for HipChat'
@@ -27,19 +29,22 @@ class NotifyHipchatPlugin(Plugin2):
     conf_key = 'notify-hipchat'
     project_conf_form = NotifyHipchatForm
 
-    def get_notifiers(self, **kwargs):
-        return [HipchatNotifier]
+    def post_process(self, group, event, is_new, is_sample, **kwargs):
+        if self.__should_notify(group, event, is_new):
+            self.__notify(group, event, is_new)
 
+    def __should_notify(self, group, event, is_new):
+        if self.get_option('new_only', group.project) and not is_new:
+            return False
+        if self.get_option('ignore_muted', group.project) and group.is_muted():
+            return False
+        return True
 
-class HipchatNotifier(Notifier):
-    slug = 'notify-hipchat'
-
-    def notify(self, notification, **kwargs):
-        event = notification.event
-        group = event.group
-
+    def __notify(self, group, event, is_new):
         project_name = '<strong>{0}</strong>'.format(saxutils.escape(group.project.name))
-        times_seen = group.times_seen
+        times_seen = group.times_seen+1
+        if is_new:
+            times_seen = 1
 
         message = '''[{level}] {project} {message}<br>
         {culprit} ({count} times seen)<br>
